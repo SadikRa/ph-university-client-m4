49da0c30de6c598bf584aa1251aadf79f0bb1ffb
SadikRa <sadikrahman494@gmail.com>
2025-01-09 21:05:36 +0600

    today done

--- src/pages/admin/CreateFaculty.tsx
+++ src/pages/admin/CreateFaculty.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 const CreateFaculty = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div></div>;
 };
 
 export default CreateFaculty;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,61 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateStudent from "../pages/admin/CreateStudent";
+import { adminPaths } from "./admin.routes";
 import AdminLayout from "../components/layout/AdminLayout";
-import CreateFaculty from "../pages/admin/CreateFaculty";
-import CreateAdmin from "../pages/admin/CreateAdmin";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
-    children: [
-      {
-        path: "about",
-        element: <About></About>,
-      },
-      {
-        path: "contact",
-        element: <Contact></Contact>,
-      },
-    ],
-  },
-
-  {
-    path: "/login",
-    element: <Login></Login>,
-  },
-  {
-    path: "/register",
-    element: <Register></Register>,
   },
 
   {
     path: "/admin",
     element: <AdminLayout></AdminLayout>,
-    children: [
-      {
-        path: "dashboard",
-        element: <AdminDashboard></AdminDashboard>,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent></CreateStudent>,
-      },
-      {
-        path: "create-faculty",
-        element: <CreateFaculty></CreateFaculty>,
-      },
-      {
-        path: "create-admin",
-        element: <CreateAdmin></CreateAdmin>,
-      },
-    ],
+    children: adminPaths,
+  },
+  {
+    path: "/faculty",
+    element: <AdminLayout></AdminLayout>,
+    children: adminPaths,
+  },
+  {
+    path: "/student",
+    element: <AdminLayout></AdminLayout>,
+    children: adminPaths,
   },
 
   {
