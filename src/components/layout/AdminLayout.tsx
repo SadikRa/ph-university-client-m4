b1d3ba44b300303a94ed6b0b80b38081c224ad4c
SadikRa <sadikrahman494@gmail.com>
2025-01-09 16:56:03 +0600

    admin layout added done

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const AdminLayout = () => {
   return (
     <div>
       <h1>admin layout should be here </h1>
+      <Outlet></Outlet>
     </div>
   );
 };
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,59 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateStudent from "../pages/admin/CreateStudent";
+import AdminLayout from "../components/layout/AdminLayout";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App></App>
-    }
-])
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "about",
+        element: <About></About>,
+      },
+      {
+        path: "contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+
+  {
+    path: "/admin",
+    element: <AdminLayout></AdminLayout>,
+    children: [
+      {
+        path: "dashboard",
+        element: <AdminDashboard></AdminDashboard>,
+      },
+      {
+        path: "create-student",
+        element: <CreateStudent></CreateStudent>,
+      },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+]);
