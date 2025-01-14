2cb3fd3616bc4c04de6bed0027ccf195ba777ff2
SadikRa <sadikrahman494@gmail.com>
2025-01-14 22:27:25 +0600

    auth store file added done

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,59 +5,53 @@ import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
 import { facultyPaths } from "../../routes/faculty.routes";
 import { studentPaths } from "../../routes/student.routes";
 
-
 const userRole = {
-  ADMIN: 'admin',
-  FACULTY: 'faculty',
-  STUDENT: 'student'
-}
+  ADMIN: "admin",
+  FACULTY: "faculty",
+  STUDENT: "student",
+};
 
 const Sidebar = () => {
-
-  const role = 'admin' 
+  const role = "admin";
 
   let sidebarItems;
 
   switch (role) {
     case userRole.ADMIN:
-      sidebarItems = sidebarItemsGenerator(adminPaths,  userRole.ADMIN) 
+      sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
       break;
     case userRole.FACULTY:
-      sidebarItems = sidebarItemsGenerator(facultyPaths,  userRole.FACULTY) 
+      sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY);
       break;
     case userRole.STUDENT:
-      sidebarItems = sidebarItemsGenerator(studentPaths,  userRole.STUDENT) 
+      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT);
       break;
-  
+
     default:
       break;
   }
 
-    return (
-        <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        
+  return (
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div
+        style={{
+          color: "white",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <div
-          style={{
-            color: "white",
-            textAlign: "center",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          PH university
-        </div>
+        PH university
+      </div>
 
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={sidebarItems}
-        />
-      </Sider>
-    );
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={sidebarItems}
+      />
+    </Sider>
+  );
 };
 
 export default Sidebar;
